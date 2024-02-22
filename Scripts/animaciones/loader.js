b919dd84131dc4359e227685ab52089a44abc160
Alexis <220240@fppiramide.com>
2024-02-22 13:14:21 +0100

    Update

---
+++ Scripts/animaciones/loader.js
@@ -0,0 +1,6 @@
+setTimeout(function() {
+    if (document.getElementById('loader-container') !== null) {
+      document.getElementById('loader-container').style.display = 'none';
+      document.querySelector('.content').style.display = 'contents';
+    }
+  }, 1500);
